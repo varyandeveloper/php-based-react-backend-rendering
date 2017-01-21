0eb93a52a9ad3d748409d76f9e34401d4a06bb5b
Varazdat Stepanyan <developervaryan@gmail.com>
2017-01-21 16:14:57 +0400

    Added functionality

--- src/React.php
+++ src/React.php
@@ -8,5 +8,29 @@ namespace VarYans\ReactPHP;
  */
 class React
 {
+    /**
+     * @var \ReactJS $_react
+     */
+    protected $_react;
 
+    /**
+     * React constructor.
+     * @param string $reactSource
+     * @param string $appSource
+     */
+    public function __construct($reactSource,$appSource)
+    {
+        $this->_react = new \ReactJS($reactSource,$appSource);
+    }
+
+    /**
+     * @param string $component
+     * @param array $data
+     * @return string
+     */
+    public function render($component,$data)
+    {
+        $this->_react->setComponent($component,$data);
+        return $this->_react->getMarkup();
+    }
 }
